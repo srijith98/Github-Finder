8cea667262e6d0f02136323ec5158e4c97dbd41d
srijith98 <44927744+srijith98@users.noreply.github.com>
2020-02-17 10:53:30 +0530

    Update github.js

--- github.js
+++ github.js
@@ -1,7 +1,7 @@
 class GitHub {
     constructor() {
-        this.client_id = '65fbf690c7e0f19b81b0';
-        this.client_secret = '6aeda62185e899131012a8215af81801145ed947';
+        this.client_id = ;
+        this.client_secret = ;
         this.repos_count = 5;
         this.repos_sort = 'created: asc';
     }
